ec5bed187079cf779dacbac512f91844e60c7959
NolaZheng <mania001988@gmail.com>
2020-05-20 18:42:47 +0800

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,93 +1,101 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">StockTrader</router-link>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <router-link to="/" class="navbar-brand">StockTrader</router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" active-class="active" tag="li"
+          ><a class="nav-link">Portfolio</a></router-link
+        >
+        <router-link to="/stocks" active-class="active" tag="li"
+          ><a class="nav-link">Stocks</a></router-link
+        >
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <router-link to="/portfolio" active-class="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
-                    <router-link to="/stocks" active-class="active" tag="li"><a class="nav-link">Stocks</a></router-link>
+        <strong class="navbar-text">Funds:{{ funds | currency }}</strong>
 
-                    <strong class="navbar-text">Funds:{{funds | currency}}</strong>
+        <div class="row nav-right">
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click="endDay">End Day</a>
+          </li>
 
-                    <div class="row nav-right">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" @click="endDay">End Day</a>
-                        </li>
+          <div class="nav-item dropdown">
+            <!-- <a
+              class="nav-link dropdown-toggle"
+              @click="isActive = !isActive"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Save & Load
+            </a> -->
 
-                        <div class="nav-item dropdown">
-                            <a 
-                            class="nav-link dropdown-toggle" 
-                            @click="isActive = !isActive"
-                            href="#" id="navbarDropdown" 
-                            role="button" 
-                            data-toggle="dropdown" 
-                            aria-haspopup="true" 
-                            aria-expanded="false">
-                            Save & Load
-                            </a>
-                        
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#" @click="saveData">Save Data</a>
-                                <a class="dropdown-item" href="#" @click="loadData">Load Data</a>
-                            </div>
-                            
-
-                        </div>
-                    </div>
-                    
-                </ul>
-            
-            </div>
-        </nav>
-
+            <b-nav-item-dropdown
+              id="my-nav-dropdown"
+              text="Save & Load"
+              toggle-class="nav-link-custom"
+              right
+            >
+              <b-dropdown-item class="dropdown-item" href="#" @click="saveData"
+                >Save Data</b-dropdown-item
+              >
+              <b-dropdown-item class="dropdown-item" href="#" @click="loadData"
+                >Load Data</b-dropdown-item
+              >
+            </b-nav-item-dropdown>
+          </div>
+        </div>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-import {mapActions} from 'vuex';
+import { mapActions } from 'vuex'
 export default {
-    data() {
-        return{
-            isActive: false
-        }
-    },
-    computed: {
-        funds() {
-            return this.$store.getters.funds;
-        }
-    },
-    methods: {
-        ...mapActions({
-            randomizeStocks:'randomizeStocks',
-            fetchData:'loadData'
-        }),
-        endDay() {
-            this.randomizeStocks();
-        },
-        saveData() {
-            const data = {
-                funds: this.$store.getters.funds,
-                stockPortfolio: this.$store.getters.stockPortfolio,
-                stocks: this.$store.getters.stocks
-            };
-            this.$http.put('data.json',data);
-            
-        },
-        loadData() {
-            this.fetchData();
-        }
+  data() {
+    return {
+      isActive: false,
     }
+  },
+  computed: {
+    funds() {
+      return this.$store.getters.funds
+    },
+  },
+  methods: {
+    ...mapActions({
+      randomizeStocks: 'randomizeStocks',
+      fetchData: 'loadData',
+    }),
+    endDay() {
+      this.randomizeStocks()
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks,
+      }
+      this.$http.put('data.json', data)
+    },
+    loadData() {
+      this.fetchData()
+    },
+  },
 }
 </script>
 
 <style scoped>
-    .nav-right {
-        position: absolute;
-        right: 30px;
-        margin-right: 10px;
-    }
-    .navbar-text {
-        position: absolute;
-        left: 45%;
-    }
+.nav-right {
+  position: absolute;
+  right: 30px;
+  margin-right: 10px;
+}
+.navbar-text {
+  position: absolute;
+  left: 45%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
 
 import App from './App.vue'
-import {routes} from './routes';
-import store from './store/store';
+import { routes } from './routes'
+import store from './store/store'
 
-Vue.use(VueRouter);
-Vue.use(VueResource);
+Vue.use(VueRouter)
+Vue.use(VueResource)
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
 
-Vue.http.options.root = 'https://vuejs-stock-trader-d1691.firebaseio.com/';
+Vue.http.options.root = 'https://vuejs-stock-trader-d1691.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
-  return '$' + value.toLocaleString();
-});
+  return '$' + value.toLocaleString()
+})
 
 const router = new VueRouter({
   mode: 'history',
@@ -24,5 +34,5 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 })
