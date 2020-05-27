db513c87007c96159683abf259ef9b0f8e931ff6
NolaZheng <mania001988@gmail.com>
2020-05-27 15:11:57 +0800

    deploy

---
+++ precache-manifest.06668196bc140f0dd6a8c749f9645f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266772ec3a26c3daf1da",
+    "url": "/stock-trader/css/app.662140ee.css"
+  },
+  {
+    "revision": "84afdb190fd6710a7cb5",
+    "url": "/stock-trader/css/chunk-vendors.94bc0478.css"
+  },
+  {
+    "revision": "adc92f80eb4e5128bd40ec2b9b1d9fa8",
+    "url": "/stock-trader/index.html"
+  },
+  {
+    "revision": "266772ec3a26c3daf1da",
+    "url": "/stock-trader/js/app.bb62c18b.js"
+  },
+  {
+    "revision": "84afdb190fd6710a7cb5",
+    "url": "/stock-trader/js/chunk-vendors.a4c104ee.js"
+  },
+  {
+    "revision": "22bdfa816f18d2cd6367dd7987f731b1",
+    "url": "/stock-trader/manifest.json"
+  }
+]);
